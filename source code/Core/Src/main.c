6a5861b23efc44b4612b0c7e147b5fee4e5c183c
quangisthebest <99035741+quangisthebest@users.noreply.github.com>
2025-10-08 13:41:25 +0700

    do-ex6

--- source code/Core/Src/main.c
+++ source code/Core/Src/main.c
@@ -61,6 +61,11 @@ static void MX_GPIO_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
+const uint16_t clockPins[] = {
+    GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7,
+    GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_11,
+    GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15
+};
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -94,12 +99,12 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  for (int i = 4; i <= 15; i++) {
+	  for (int i = 0; i <= 11; i++) {
 
-	      HAL_GPIO_WritePin(GPIOA, 1<<i, GPIO_PIN_RESET);
+	      HAL_GPIO_WritePin(GPIOA, clockPins[i], GPIO_PIN_RESET);
 	      HAL_Delay(500);
 
-	      HAL_GPIO_WritePin(GPIOA, 1<<i, GPIO_PIN_SET);
+	      HAL_GPIO_WritePin(GPIOA, clockPins[i], GPIO_PIN_SET);
 	    }
     /* USER CODE BEGIN 3 */
   }
